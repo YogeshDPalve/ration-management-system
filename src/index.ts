f3b4e27ddecc6deae8f64d7ac05b7a5cae001dbb
Yogesh Palve <dnyyogesh@gmail.com>
2025-03-27 01:36:04 +0530

    user routes file created

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import chalk from "chalk";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import userRoutes from "./Routes/user.routes";
 // configure dotenv
 dotenv.config();
 
@@ -16,6 +17,9 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(cookieParser());
+
+// generating routes
+app.use("/api/v1/user", userRoutes);
 
 // importing some data from .env file
 const port = process.env.PORT;
