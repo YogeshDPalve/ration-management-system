1f3141d5d53baf94e6129d62fc335bd6f32d4687
Yogesh Palve <dnyyogesh@gmail.com>
2025-03-27 11:26:24 +0530

    minor schema change and checking unique fields middleware added

--- src/Controllers/user.controller.ts
+++ src/Controllers/user.controller.ts
@@ -1,8 +1,48 @@
-import {  Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+
+// prisma initiation
+const prisma = new PrismaClient();
 
 // register user controller
-const registerUser = (req: Request, res: Response) => {
+const registerUser = async (req: Request, res: Response): Response<unknown> => {
+  try {
+    const {
+      rationId, // primary
+      adharcardNumber, //unique
+      firstName,
+      middleName,
+      lastName,
+      mobileNo, //unique
+      email,
+      Address,
+      password,
+    } = req.body;
 
+    const salt: string = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+      data: {
+        rationId,
+        adharcardNumber,
+        firstName,
+        middleName,
+        lastName,
+        mobileNo,
+        email,
+        Address,
+        password,
+      },
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: "Internal server error in register controller",
+      error,
+    });
+  }
 };
 // login user controller
 const loginUser = () => {};
--- src/Routes/user.routes.ts
+++ src/Routes/user.routes.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 import { loginUser, registerUser } from "../Controllers/user.controller";
 import { registerValidation } from "../Middlewares/userValidatoin";
 import { ValidationChain } from "express-validator";
+import checkUnique from "../Middlewares/checkUniqueFields";
 
 const router = Router();
 
-router.post("/register", registerValidation as ValidationChain[], registerUser);
+router.post(
+  "/register",
+  registerValidation as ValidationChain[],
+  checkUnique,
+  registerUser
+);
 router.post("/login", loginUser);
 
 export default router;
