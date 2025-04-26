931b1998576ff891aaba02ec28558a164c943e16
Yogesh Palve <dnyyogesh@gmail.com>
2025-04-26 23:11:50 +0530

    new ration route file created, data base changes in ration notification and new routes created for ration

--- src/Controllers/ration.controller.ts
+++ src/Controllers/ration.controller.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
-import { AuthRequest } from "../Constants/interfaces";
+import { purchaseHistoryBody } from "../Constants/interfaces";
+import { AllotRation } from "../Constants/types";
 
 // create instance of prisma
 const prisma = new PrismaClient();
 
 // get ration details controller
-const getRationDetails = async (
-  req: AuthRequest,
-  res: Response
-): Promise<any> => {
+const getRationDetails = async (req: Request, res: Response): Promise<any> => {
   try {
-    const rationId: string = req.info?.rationId as string;
+    const { rationId } = req.body;
     if (!rationId) {
       return res.status(400).send({
         success: false,
@@ -33,11 +31,115 @@ const getRationDetails = async (
       message: "Ration Details fetched successfully",
     });
   } catch (error) {
+    console.log(error);
     return res.status(500).send({
       success: false,
       message: "Internal server error to get ration details",
+      error,
     });
   }
 };
+
 // allot ration details controller
-export { getRationDetails };
+const allotRation = async (req: Request, res: Response) => {
+  try {
+    const {
+      rationId,
+      wheatQuota,
+      riceQuota,
+      sugarQuota,
+      daalQuota,
+      oilQuota,
+    }: AllotRation = req.body;
+     
+
+    const checkUser = await prisma.user.findUnique({
+      where: { rationId },
+    });
+    if (!checkUser) {
+      return res.status(400).send({
+        success: false,
+        message: "Provided ration id is invalid",
+      });
+    }
+
+    const rationAllot = await prisma.rationAllotment.create({
+      data: {
+        rationId,
+        wheatQuota,
+        riceQuota,
+        sugarQuota,
+        daalQuota,
+        oilQuota,
+      },
+    });
+
+    const sendNotification = await prisma.rationNotification.create({
+      data: {
+        rationId,
+        
+      }
+    })
+    return res.status(201).send({
+      success: true,
+      message: "Ration allotted successfully",
+      data: rationAllot,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Failed to allot ration details",
+      error,
+    });
+  }
+};
+
+const rationPurchased = async (req: Request, res: Response) => {
+  try {
+    const {
+      rationId,
+      wheatPurchased,
+      ricePurchased,
+      sugarPurchased,
+      daalPurchased,
+      oilPurchased,
+      fpsShopNumber,
+    }: purchaseHistoryBody = req.body;
+
+    const checkUser = await prisma.user.findUnique({ where: { rationId } });
+    if (!checkUser) {
+      return res.status(400).send({
+        success: false,
+        message: "User Not found",
+      });
+    }
+
+    const purchaseData = await prisma.purchaseHistory.create({
+      data: {
+        rationId,
+        wheatPurchased,
+        ricePurchased,
+        sugarPurchased,
+        daalPurchased,
+        oilPurchased,
+        fpsShopNumber,
+      },
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "Ration purchased successfully",
+      purchaseData,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Failed to purchase ration",
+    });
+  }
+};
+
+export { getRationDetails, allotRation, rationPurchased };
+8;
