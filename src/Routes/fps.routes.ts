8b1689d19b0824f6ed5682a2a74ffd7f6e9cec3a
Yogesh Palve <dnyyogesh@gmail.com>
2025-04-29 12:49:50 +0530

    Admin and fps input validations functions created

--- src/Routes/fps.routes.ts
+++ src/Routes/fps.routes.ts
@@ -4,8 +4,8 @@ import { authFPSMiddleware } from "../Middlewares/authAdminMiddleware";
 
 const route = Router();
 
-route.post("/login", fpsLogin); // fair price shop login route
 route.post("/register", fpsRegister); // fair price shop register route
+route.post("/login", fpsLogin); // fair price shop login route
 route.post("/add", authFPSMiddleware); // fair price shop register route
 
 export default route;
